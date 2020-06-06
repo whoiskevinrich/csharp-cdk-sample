2ce5903f2891ff7bcc4434362c243463ce22bceb
Kevin Rich <whoiskevinrich@gmail.com>
2020-06-06 10:14:31 -0700

    init cdk

---
+++ cdk-infrastructure/src/CdkInfrastructure/CdkInfrastructureStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace CdkInfrastructure
+{
+    public class CdkInfrastructureStack : Stack
+    {
+        internal CdkInfrastructureStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
---
+++ cdk-infrastructure/src/CdkInfrastructure/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CdkInfrastructure
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new CdkInfrastructureStack(app, "CdkInfrastructureStack");
+            app.Synth();
+        }
+    }
+}
